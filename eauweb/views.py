4fc3769fb6bee9a277f07645cacab5c1c2213fcb
Raymond Ng <raymondng@ocf.berkeley.edu>
2017-10-26 20:55:03 -0700

    add food section and move pages

--- eauweb/views.py
+++ eauweb/views.py
@@ -8,6 +8,10 @@ from db import app
 def index():
    return render_template('index.html')
 
+@app.route('/nightmarket')
+def nightmarket():
+    return render_template('nightmarket.html')
+
 @app.route('/officers')
 def officers():
     return render_template('officers.html')

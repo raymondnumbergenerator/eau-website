af98fec19892cde734907e78e9290cefc219d6fd
Raymond Ng <rng@Raymonds-MacBook-Pro.local>
2017-10-27 14:04:39 -0700

    add helper function for later and beautify code

---
+++ eauweb/helper.py
@@ -0,0 +1,27 @@
+import random
+
+MDL_COLORS = [
+    '.mdl-color--red',
+    '.mdl-color--red-900',
+    '.mdl-color--red-A400',
+    '.mdl-color--pink',
+    '.mdl-color--pink-200',
+    '.mdl-color--purple',
+    '.mdl-color--purple-A200',
+    '.mdl-color--indigo',
+    '.mdl-color--blue',
+    '.mdl-color--light-blue',
+    '.mdl-color--light-A400',
+    '.mdl-color--teal',
+    '.mdl-color--green',
+    '.mdl-color--green-A400',
+    '.mdl-color--light-green',
+    '.mdl-color--lime',
+    '.mdl-color--yellow',
+    '.mdl-color--amber',
+    '.mdl-color--orange',
+    '.mdl-color--blue-grey'
+]
+
+def random_mdl_color():
+    return random.choice(MDL_COLORS)
